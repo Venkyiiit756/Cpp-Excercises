c799a6b702de5294645e3649d8f78285f7836adc
Venkyiiit756 <38283554+Venkyiiit756@users.noreply.github.com>
2020-04-10 19:30:51 +0530

    Single Linked list operations
    Following are implemented:
    1) Add a node the beginning.
    2) Add a node after the given value.
    3) Add a node at the end of the list.
    4) Traverse and Print the list.
    5) Reverse the list
    6) Delete the node for give value(Beginning case also included)

---
+++ LinkedListOps.cpp
@@ -0,0 +1,155 @@
+#include<iostream>
+#include<stdint.h>
+
+
+struct Node
+{
+    int m_data;
+    Node * m_next;
+    Node* m_head;
+    
+    
+    Node():m_data(0),m_head(nullptr) {}
+    
+    void insertFront(const int& p_data) 
+    {
+        Node* temp = new Node;
+        temp -> m_data = p_data; 
+        temp -> m_next = m_head;
+        m_head = temp;
+    }
+    
+    void printList()
+    {
+        Node* ptr = m_head;
+        while(ptr != 0)
+        {
+            std::cout << ptr->m_data << " ";
+            ptr = ptr -> m_next;
+        }
+        std::cout << std::endl;
+    }
+    
+    void pushBack(const int& p_data)
+    {
+        // Create the new node & add only data
+        Node* temp = new Node;
+        temp -> m_data = p_data;
+       
+        //Go to the last element
+        Node* ptr = m_head;
+        while(ptr->m_next != nullptr)
+        {
+            ptr = ptr -> m_next;    
+        }
+        
+        std::cout << "Sad!\n";
+        ptr -> m_next = temp;
+        temp -> m_next = nullptr;
+    }
+    void randomAfterNode(int p_cData, int p_nData)
+    {
+        Node* ptr = m_head;
+        
+        while(ptr->m_data != p_cData)
+        {
+            
+            ptr = ptr->m_next;
+            
+        }
+          
+        //Crate the new node
+        Node* temp = new Node;
+        temp -> m_data = p_nData;
+        
+        // Establis the links
+        temp -> m_next = ptr-> m_next;
+        
+        ptr-> m_next = temp;
+        
+    }
+    
+    void reverse()
+    {
+        // starting node
+        Node* start = m_head; 
+        
+        // ending node
+        Node* end = m_head;
+        while(end -> m_next != nullptr)
+        {
+            end = end -> m_next;
+        } 
+        
+        while(start != end)
+        {
+           if(end -> m_next == start) 
+           {
+                break;    
+           }
+            // Swap the start and end  
+            int temp = start -> m_data;
+            start -> m_data = end -> m_data;
+            end -> m_data = temp;
+            
+            // Increment the start and decrement the end
+            start = start -> m_next;
+            
+            Node* tPtr = m_head;
+            while(tPtr -> m_next != end)
+            {
+                tPtr = tPtr -> m_next; 
+            }
+            end = tPtr;
+        }
+        
+    }
+    
+    
+    void deleteNodeByValue(int p_data)
+    {
+        Node *cur = m_head;
+        Node *prev = nullptr;
+        
+        // Handle the first node deletion
+        if(m_head->m_data == p_data)
+        {
+            m_head = cur->m_next; 
+            return;
+        }
+        
+        while(cur -> m_data != p_data)
+        {
+            prev = cur;
+            cur = cur -> m_next;    
+        }
+        
+        // Remove the link
+        prev -> m_next = cur -> m_next;
+        
+        delete cur;
+    }
+        
+};
+
+
+int main()
+{
+    
+    Node list;
+    list.insertFront(2);
+    list.insertFront(1);
+    list.pushBack(3);
+    list.pushBack(12);
+    list.pushBack(14);
+    list.randomAfterNode(2, 10);
+    list.printList();
+    list.reverse();
+    list.printList();
+    
+    list.deleteNodeByValue(1);
+    list.deleteNodeByiValue(10);
+    list.deleteNodeByValue(14);
+    list.printList();
+    
+}
